42ecac14fc52a42f00aa932a84aa642f956799d0
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-20 19:12:29 -0400

    refactored to have a master master tabBox and looser coupling

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -2,31 +2,56 @@
 (() => {
   function newTabItem({ _element }) {
     const select = () => {
-      document.querySelectorAll('.tabs .tabs-item').forEach((item) => {
-        item.classList.remove('tabs-item-selected');
-      });
       _element.classList.add('tabs-item-selected');
     };
 
-    return Object.freeze({ select });
+    const deselect = () => {
+      _element.classList.remove('tabs-item-selected');
+    };
+
+    return Object.freeze({ select, deselect });
   }
 
-  function newTabLink({ _element }) {
-    const _tabItem = newTabItem({
-      _element: document
-        .querySelector(`.tabs-items .tabs-item[data-tab='${_element.dataset.tab}']`),
-    });
+  function newTabLink({ _element, _setNewCurrentTab }) {
+    const _tabItemElement = document.querySelector(
+      `.tabs-items .tabs-item[data-tab='${_element.dataset.tab}']`,
+    );
+    const _tabItem = newTabItem({ _element: _tabItemElement });
+
+    const updateSelectedStatus = (event) => {
+      if (Array.from(event.currentTarget.classList).includes('tabs-link-selected')) return;
+      _setNewCurrentTab({ currentTarget: event.currentTarget });
+    };
 
     const select = () => {
-      document.querySelectorAll('.tabs .tabs-link').forEach((link) => {
-        link.classList.remove('tabs-link-selected');
-      });
       _element.classList.add('tabs-link-selected');
       _tabItem.select();
     };
 
-    _element.addEventListener('click', select);
+    const deselect = () => {
+      _element.classList.remove('tabs-link-selected');
+      _tabItem.deselect();
+    };
+
+    _element.addEventListener('click', updateSelectedStatus);
+
+    return Object.freeze({ select, deselect });
   }
 
-  document.querySelectorAll('.tabs-link').forEach(link => newTabLink({ _element: link }));
+  function newTabBox({ _element }) {
+    const _tabLinkElementObjWeakMap = new WeakMap();
+    let _currentTab = _element.getElementsByClassName('tabs-link-selected')[0];
+
+    const _setNewCurrentTab = ({ currentTarget }) => {
+      _tabLinkElementObjWeakMap.get(_currentTab).deselect();
+      _tabLinkElementObjWeakMap.get(currentTarget).select();
+      _currentTab = currentTarget;
+    };
+
+    _element.querySelectorAll('.tabs-links .tabs-link').forEach((link) => {
+      _tabLinkElementObjWeakMap.set(link, newTabLink({ _element: link, _setNewCurrentTab }));
+    });
+  }
+
+  newTabBox({ _element: document.querySelector('.tabs') });
 })();
